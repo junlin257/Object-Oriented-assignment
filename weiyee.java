3f0f5fb9656409be85e4765804b1ebbfbec36fcd
MrKoh7 <111169101+MrKoh7@users.noreply.github.com>
2023-08-14 21:32:11 +0800

    Update weiyee.java

--- weiyee.java
+++ weiyee.java
@@ -66,3 +66,321 @@ public class SeatsAvailable {
 	
 }
 
+
+//Main for seatsAvailability
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to WayToHell Train Booking System");
+		Scanner scanner = new Scanner (System.in);
+		
+		
+		System.out.println("There Are 300 Seats Available For Each Standard Class Train in Total");
+		System.out.println("There Are 150 Seats Available For Each Premium Class Train in Total");
+		System.out.println();
+		
+		int options;
+		
+		do {
+			System.out.print("Enter Number 1 to book Standard Class/Number 2 for Premium Class Train : ");
+			options = scanner.nextInt();
+			
+			if(options != 1 && options != 2) {
+				System.out.println("Invalid Options!Please Choose Again");
+			}
+				
+		} while (options != 1 && options != 2);
+		
+		SeatsAvailable standardSeats = null;
+		SeatsAvailable premiumSeats = null;
+		
+		if(options == 1) {
+			
+			standardSeats = new SeatsAvailable(300);
+			
+			int chooseStandardTrain;
+			
+			do {
+				standardSeats.displayStandardTrainList();
+				System.out.print("Choose a Train Slot: ");
+				chooseStandardTrain = scanner.nextInt();
+				
+				if(chooseStandardTrain <=0 || chooseStandardTrain > 15) {
+					System.out.println("Invalid Slot!Please Choose Again");
+				}
+				
+			} while(chooseStandardTrain <= 0 || chooseStandardTrain > 15);
+			
+			
+			
+			System.out.print("Enter the Total Number of Seats: ");
+			int numSeats = scanner.nextInt();
+			if(standardSeats.bookSeats(numSeats)) {
+				System.out.println("Seats Booked Successfully. Enjoy:)");
+				System.out.println("Available Seats: " + standardSeats.getAvailableSeats());
+			} else {
+				System.out.println("Invlid Booking Request!");
+			}
+			
+		}
+		else if (options == 2) {
+			premiumSeats = new SeatsAvailable(150);
+			
+			int choosePremiumTrain;
+			
+			do {
+				premiumSeats.displayPremiumTrainList();
+				System.out.println("Choose your Train and Time: ");
+				choosePremiumTrain = scanner.nextInt();
+				
+				if(choosePremiumTrain <=0 || choosePremiumTrain >10) {
+					System.out.println("Invalid Slot!Please Choose again!");
+				}
+				
+			}while(choosePremiumTrain <=0 || choosePremiumTrain >10);	
+				
+				
+			System.out.println("Enter the Total Number of Seats: ");
+			int numSeats = scanner.nextInt();
+			if(premiumSeats.bookSeats(numSeats)) {
+				System.out.println("Seats Booked Successfully.Enjoy:)");
+				System.out.println("Available Seats: " + premiumSeats.getAvailableSeats());
+			}
+			
+		} else {
+			System.out.println("Invalid Input!");
+		}
+		
+		System.out.println();
+		
+		
+		System.out.print("Would You Like to Cancel Your Booking?: ");
+		scanner.nextLine();
+		String cancelOption = scanner.nextLine();
+		
+		
+			if(cancelOption.equalsIgnoreCase("yes")) {
+				String trainClass;
+			do {
+				System.out.println("Enter Train Class (standard/premium): ");
+				trainClass = scanner.nextLine();
+				
+				if(!trainClass.equalsIgnoreCase("standard") && !trainClass.equalsIgnoreCase("premium")) {
+					System.out.println("Invalid Train Class!Please Enter Again");
+				}
+			}while(!trainClass.equalsIgnoreCase("standard") && !trainClass.equalsIgnoreCase("premium"));
+		
+			if(trainClass.equalsIgnoreCase("standard")) {
+				standardSeats.displayStandardTrainList();
+				String optionCancel;
+				
+				do{
+					System.out.println("Choose the Train Slot You have Booked Previously: ");
+					int cancelTrain = scanner.nextInt();
+					System.out.println("Enter the Number of Seats to Cancel: ");
+					int cancelSeats = scanner.nextInt();
+					
+					System.out.println("Are You Sure to Cancel the Following Train Slot? (Yes/No): ");
+					System.out.println("Train Slot: " + cancelTrain);
+					System.out.println("Number of Seats: " + cancelSeats);
+					optionCancel = scanner.next();
+				
+				
+					if(optionCancel.equalsIgnoreCase("yes")) {
+						standardSeats.cancelSeats(cancelSeats);
+						System.out.println("Seats Cancelled Successfully");
+						System.out.println("Available Seats after cancellation: " + standardSeats.getAvailableSeats());
+					}else if(optionCancel.equalsIgnoreCase("no")) {
+						System.out.println("Please Choose Again the Train Slot to Cancel");
+					}else {
+						System.out.println("Invalid Input!");
+					}
+					
+				}while(!optionCancel.equalsIgnoreCase("yes"));
+				
+				
+			} else if (trainClass.equalsIgnoreCase("premium")){
+				premiumSeats.displayPremiumTrainList();
+				String optionCancel;
+				
+				do {
+					System.out.println("Choose the Train Slot You have Booked Previously: ");
+					int cancelTrain = scanner.nextInt();
+					System.out.println("Enter the Number of Seats to Cancel: ");
+					int cancelSeats = scanner.nextInt();
+					
+					System.out.println("Are You Sure to Cancel the Following Train Slot? (Yes/No): ");
+					System.out.println("Train Slot: " + cancelTrain);
+					System.out.println("Number of Seats: " + cancelSeats);
+					optionCancel = scanner.next();
+					
+					if(optionCancel.equalsIgnoreCase("yes")) {
+						premiumSeats.cancelSeats(cancelSeats);
+						System.out.println("Seats Cancelled Successfully");
+						System.out.println("Available Seats after cancellation: " + premiumSeats.getAvailableSeats());
+					} else if(optionCancel.equalsIgnoreCase("no")) {
+						System.out.println("Please Choose Again the Train Slot to Cancel");
+					} else {
+						System.out.println("Invalid Input!");
+					}
+				}while(!optionCancel.equalsIgnoreCase("yes"));
+				
+			} else {
+				return;
+			}
+			
+		} else {
+			System.out.println("Invalid Input!");
+		}
+		
+		
+		scanner.close();
+
+	}
+	
+}
+
+
+//FareCalculation
+
+
+
+public class trainFare {
+	
+	private static final double STANDARD_FARE_PER_RATE = 0.20;
+	private static final double STANDARD_FARE_PER_RATE_PEAK = 0.30;
+	private static final double PREMIUM_FARE_PER_RATE = 0.50;
+	private static final double PREMIUM_FARE_PER_RATE_PEAK = 0.70;
+	private static final double DISTANCE_WITHIN = 3;
+	private static final double DISTANCE_OUTSIDE = 5;
+	
+	private double trainFee;
+	private int time;
+	private String trainType;
+	private double distance;
+	
+	public double getTrainFee() {
+		return trainFee;
+	}
+	
+	public int getTime() {
+		return time;
+	}
+	
+	public String getTrainType() {
+		return trainType;
+	}
+	
+	public double distance() {
+		return distance;
+	}
+
+	public static boolean peakTime(int time) {
+		return (time >= 600 && time <= 900);
+	}
+	
+	public trainFare(String trainType, int time, double distance) {
+		this.trainType = trainType;
+		this.time = time;
+		this.distance = distance;
+	}
+	
+	public void calculateTrainFare() {
+		double farePerRate = 0.0;
+		
+		if(trainType.equalsIgnoreCase("standard")) {
+			farePerRate = peakTime(time) ? STANDARD_FARE_PER_RATE_PEAK : STANDARD_FARE_PER_RATE;
+		} else if (trainType.equalsIgnoreCase("premium")){
+			farePerRate = peakTime(time) ? PREMIUM_FARE_PER_RATE_PEAK : PREMIUM_FARE_PER_RATE;
+		} else {
+			System.out.println("Invalid Train Class");
+		}
+		
+		double distanceThreshold1 = (trainType.equalsIgnoreCase("standard")) ? DISTANCE_WITHIN : DISTANCE_OUTSIDE;
+		double distanceThreshold2 = (trainType.equalsIgnoreCase("premium")) ? DISTANCE_WITHIN : DISTANCE_OUTSIDE;
+		
+		if(distance <= distanceThreshold1) {
+			trainFee = distance * farePerRate;
+		} else {
+			trainFee = distanceThreshold1 * farePerRate + (distance - distanceThreshold1) * 1.5;
+		}
+		
+		if(distance <= distanceThreshold2) {
+			trainFee = distance * farePerRate;
+		} else {
+			trainFee = distanceThreshold2 * farePerRate + (distance - distanceThreshold2) * 1.5;
+		}
+		
+	}
+	
+	
+	
+
+}
+
+
+//Main2 for calculateFare()
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		System.out.println("Testing Train Payment");
+		Scanner scanner = new Scanner(System.in);
+		
+		String trainType;
+		int time;
+		double distance;
+		
+		do {
+			System.out.print("Choose your Train Class: ");
+			trainType = scanner.nextLine();
+			
+			if(!trainType.equalsIgnoreCase("standard") || trainType.equalsIgnoreCase("premium")) {
+				System.out.println("Invalid Input. Please Enter only Standard/Premium Train Class");
+			}
+		}while(!trainType.equalsIgnoreCase("standard") || trainType.equalsIgnoreCase("premium"));
+		
+		do {
+			System.out.print("Choose Departure Time (24-Hour Format): ");
+			time = scanner.nextInt();
+			
+			if(time < 0 || time > 2359 || time % 100 >= 60) {
+				System.out.println("Invalid time format.Please Enter in 24-Hour Time Format.");
+			}
+		}while(time < 0 || time > 2359 || time % 100 >= 60);
+		
+		do {
+			System.out.print("Enter Distance (KM): ");
+			distance = scanner.nextDouble();
+			
+			if(distance <=0 || distance <1) {
+				System.out.println("Invalid Distance.Please Enter the Distance at least 1KM or above.");
+			}
+		}while(distance <=0 || distance <2);
+		
+		trainFare trainFare = new trainFare(trainType, time, distance);
+		trainFare.calculateTrainFare();
+		
+		DecimalFormat decimalFormat = new DecimalFormat("0.00");
+		String formattedFare = decimalFormat.format(trainFare.getTrainFee());
+		
+		System.out.println("Total Fare:");
+		System.out.println("RM" + formattedFare);
+	}
+
+}
+
+
+
+
+
+
+
+
+
