728ec60df89c74cea4e2d6f093ed0436eeaea9b0
LitJay <132117452+LitJay@users.noreply.github.com>
2023-08-17 16:01:45 +0800

    Update Filereader.java

--- Filereader.java
+++ Filereader.java
@@ -3,80 +3,96 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-public class Test2 {
-		
-	public static void main(String[] args) throws IOException {
-        List<TrainSchedule> schedules = TrainScheduleReader.readTrainSchedules("/Users/litjay/Downloads/Tumpat->Gemas.txt");
-        for (TrainSchedule schedule : schedules) {
-            System.out.println(schedule);
+
+public class TrainScheduleReader {
+    public static class TrainSchedule {
+        String trainModel;
+        List<String> arrivalTimes;
+
+        public TrainSchedule(String trainModel, List<String> arrivalTimes) {
+            this.trainModel = trainModel;
+            this.arrivalTimes = arrivalTimes;
         }
-  }
-  class TrainSchedule {
-    String trainModel;
-    List<String> arrivalTimes;
-
-    public TrainSchedule(String trainModel, List<String> arrivalTimes) {
-        this.trainModel = trainModel;
-        this.arrivalTimes = arrivalTimes;
     }
 
-    public int getStartingStationIndex() {
-        for (int i = 0; i < arrivalTimes.size(); i++) {
-            if (arrivalTimes.get(i) != null) {
-                return i;
-            }
-        }
-        return -1; // should not happen if there's at least one valid station
-    }
+    public static class ScheduleReader {
+        public static List<TrainSchedule> readTrainSchedules(String timetableFile) throws IOException {
+            List<TrainSchedule> schedules = new ArrayList<>();
 
-    @Override
-    public String toString() {
-        return "TrainSchedule{" +
-                "trainModel='" + trainModel + '\'' +
-                ", Starting Station No." + getStartingStationIndex() +
-                ", ArrivalTimes=" + arrivalTimes +
-                '}';
-    }
-}
-class TrainScheduleReader {
-    public static List<TrainSchedule> readTrainSchedules(String filename) throws IOException {
-        List<TrainSchedule> schedules = new ArrayList<>();
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-            String line;
-            StringBuilder currentTrainData = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                line = line.trim();
-                if (!line.isEmpty()) {
-                    if (line.endsWith(".")) {
-                        currentTrainData.append(line, 0, line.length() - 1);  // Remove the trailing period
-                        String[] parts = currentTrainData.toString().split(",");
+            try (BufferedReader reader = new BufferedReader(new FileReader(timetableFile))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    line = line.trim();
+                    if (!line.isEmpty()) {
+                        String[] parts = line.split(",");
                         if (parts.length > 1) {
+                            String trainModel = parts[0].trim();
                             List<String> arrivalTimes = new ArrayList<>();
                             for (int i = 1; i < parts.length; i++) {
                                 String time = parts[i].trim();
-                                if (time.equals("NA") ) {
-                                    arrivalTimes.add(null);
-                                }
-                                else if(time.equals("-"))
-                                {
-                                	arrivalTimes.add("-");
-                                }
-                                
-                                else {
-                                    arrivalTimes.add(time);
-                                }
+                                arrivalTimes.add(time);
                             }
-                            TrainSchedule schedule = new TrainSchedule(parts[0].trim(), arrivalTimes);
+                            TrainSchedule schedule = new TrainSchedule(trainModel, arrivalTimes);
                             schedules.add(schedule);
                         }
-                        currentTrainData = new StringBuilder();
-                    } else {
-                        currentTrainData.append(line).append(" ");
                     }
                 }
             }
+            return schedules;
         }
-        return schedules;
+
+        public static List<String> readStations(String filename) throws IOException {
+            List<String> stations = new ArrayList<>();
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] stationNames = line.split(",");
+                    for (String stationName : stationNames) {
+                        stations.add(stationName.trim());
+                    }
+                }
+            }
+            return stations;
+        }
+
     }
 }
+
+//callout method example
+ public void getInfo() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("From: ");
+        from = input.nextLine();
+        System.out.println("To: ");
+        to = input.nextLine();
+    }
+ public void fetchAndDisplayTrainSchedules() {
+	
+List<String> stationNames = TrainScheduleReader.ScheduleReader.readStations("/Users/litjay/Downloads/StationTumpat->Gemas.txt");
+List<TrainScheduleReader.TrainSchedule> trainSchedules = TrainScheduleReader.ScheduleReader.readTrainSchedules("/Users/litjay/Downloads/Tumpat->Gemas.txt");
+   for (int i = 0; i < stationNames.size(); i++) {
+                    if (from.equals(stationNames.get(i))) {
+                        startStationIndex = i;
+                    } else if (to.equals(stationNames.get(i))) {
+                        endStationIndex = i;
+                    }
+	 
+for (TrainScheduleReader.TrainSchedule schedule : trainSchedules) {
+	   System.out.println(schedule.trainModel);
+	   System.out.println(stationNames.get(startStationIndex));
+ 
+ 
+ 
+ }
+
+
+
+
+
+
+
+
+
+
+
