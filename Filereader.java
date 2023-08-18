5495ad571de1b229d24c57e59e7cd94b509f3bec
LitJay <132117452+LitJay@users.noreply.github.com>
2023-08-18 08:16:33 +0800

    Update Filereader.java

--- Filereader.java
+++ Filereader.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TrainScheduleReader {
+
     public static class TrainSchedule {
         String trainModel;
         List<String> arrivalTimes;
@@ -15,16 +16,18 @@ public class TrainScheduleReader {
         }
     }
 
-    public static class ScheduleReader {
-        public static List<TrainSchedule> readTrainSchedules(String timetableFile) throws IOException {
-            List<TrainSchedule> schedules = new ArrayList<>();
+    public static List<TrainSchedule> readTrainSchedules(String timetableFile) throws IOException {
+        List<TrainSchedule> schedules = new ArrayList<>();
 
-            try (BufferedReader reader = new BufferedReader(new FileReader(timetableFile))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    line = line.trim();
-                    if (!line.isEmpty()) {
-                        String[] parts = line.split(",");
+        try (BufferedReader reader = new BufferedReader(new FileReader(timetableFile))) {
+            String line;
+            StringBuilder currentTrainData = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                if (!line.isEmpty()) {
+                    if (line.endsWith(".")) {
+                        currentTrainData.append(line, 0, line.length() - 1);
+                        String[] parts = currentTrainData.toString().split(",");
                         if (parts.length > 1) {
                             String trainModel = parts[0].trim();
                             List<String> arrivalTimes = new ArrayList<>();
@@ -35,29 +38,34 @@ public class TrainScheduleReader {
                             TrainSchedule schedule = new TrainSchedule(trainModel, arrivalTimes);
                             schedules.add(schedule);
                         }
+                        currentTrainData = new StringBuilder();
+                    } else {
+                        currentTrainData.append(line).append(" ");
                     }
                 }
             }
-            return schedules;
         }
+        return schedules;
+    }
 
-        public static List<String> readStations(String filename) throws IOException {
-            List<String> stations = new ArrayList<>();
 
-            try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    String[] stationNames = line.split(",");
-                    for (String stationName : stationNames) {
-                        stations.add(stationName.trim());
-                    }
+
+    public static List<String> readStations(String filename) throws IOException {
+        List<String> stations = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] stationNames = line.split(",");
+                for (String stationName : stationNames) {
+                    stations.add(stationName.trim());
                 }
             }
-            return stations;
         }
-
+        return stations;
     }
 }
+
 
 //callout method example
  public void getInfo() {
