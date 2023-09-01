f9d2224652d2ccff917bad3de6ad98647481610b
MrKoh7 <111169101+MrKoh7@users.noreply.github.com>
2023-09-01 10:13:52 +0800

    Update weiyee.java

--- weiyee.java
+++ weiyee.java
@@ -1,3 +1,4 @@
+//SeatsAvailable
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -30,7 +31,8 @@ public class SeatsAvailable {
 			  						  {"ROW 1","COLUMN 10"},{"ROW 2","COLUMN 10"},{"ROW 3","COLUMN 10"},{"ROW 4","COLUMN 10"},{"ROW 5","COLUMN 10"}};
 	
 	private String[][] trainSeats;
-	private String bookingFilePath = "C:\\Users\\Wei Yeeeeeee\\Desktop\\booking_status.txt"; // File to store booking status
+	private String bookingFilePathA = "C:\\Users\\Wei Yeeeeeee\\Desktop\\booking_statusA.txt"; // File to store booking status
+	private String bookingFilePathB = "C:\\Users\\Wei Yeeeeeee\\Desktop\\booking_statusB.txt"; // File to store booking status
 
 	private List<String> stationNamesA;
 	private List<TrainScheduleReader.TrainSchedule> trainSchedulesA;
@@ -66,9 +68,11 @@ public class SeatsAvailable {
 	        this.availableSeats = totalSeats;
 	        
 	        trainSeats = new String[5][10];
-			initialiseSeats();
+			initialiseSeatsA();
+			initialiseSeatsB();
 			
-			loadBookingStatus();
+			loadBookingStatusA();
+			loadBookingStatusB();
 	    }
 	
 	  public void displayTrainSchedulesA() {
@@ -110,8 +114,14 @@ public class SeatsAvailable {
 			System.out.println(trainSeatsA[0] + " " + trainSeatsA[1]);
 		}
 	}
+	
+	public void displaySeatsTrainB() { 
+		for(String[] trainSeatsA : trainSeatsA) {
+			System.out.println(trainSeatsA[0] + " " + trainSeatsA[1]);
+		}
+	}
 
-	private void initialiseSeats() {
+	private void initialiseSeatsA() {
 		for(int row = 0; row < 5; row++) {
 			for(int column = 0; column < 10; column++) {
 				trainSeats[row][column] = "Available";
@@ -119,14 +129,44 @@ public class SeatsAvailable {
 		}
 	}
 	
-	public void bookSeat(int row, int column) {
-	    if (isValidSeat(row, column) && trainSeats[row - 1][column - 1].equals("Available")) {
+	private void initialiseSeatsB() {
+		for(int row = 0; row < 5; row++) {
+			for(int column = 0; column < 10; column++) {
+				trainSeats[row][column] = "Available";
+			}
+		}
+	}
+	
+	private boolean isValidSeatA(int row,int column) {
+		return row >= 1 && row <= 5 && column >= 1 && column <= 10;
+	}
+	
+	private boolean isValidSeatB(int row,int column) {
+		return row >= 1 && row <= 5 && column >= 1 && column <= 10;
+	}
+	
+	public boolean isSeatBookedA(int row, int column) {
+		if(isValidSeatA(row, column)) {
+			return trainSeats[row - 1][column -1].equals("Booked");
+		}
+		return false;
+	}
+	
+	public boolean isSeatBookedB(int row, int column) {
+		if(isValidSeatA(row, column)) {
+			return trainSeats[row - 1][column -1].equals("Booked");
+		}
+		return false;
+	}
+	
+	public void bookSeatA(int row, int column) {
+	    if (isValidSeatA(row, column) && trainSeats[row - 1][column - 1].equals("Available")) {
 	        trainSeats[row - 1][column - 1] = "Booked";
 	        System.out.println("Seat in Row " + row + ", Column " + column + " has been booked");
 
 	        // Save the updated booking status to the file
 	        try {
-	            saveBookingStatus();
+	            saveBookingStatusA();
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }
@@ -135,11 +175,32 @@ public class SeatsAvailable {
 	    }
 	}
 	
-	private boolean isValidSeat(int row,int column) {
-		return row >= 1 && row <= 5 && column >= 1 && column <= 10;
+	public void bookSeatB(int row, int column) {
+	    if (isValidSeatB(row, column) && trainSeats[row - 1][column - 1].equals("Available")) {
+	        trainSeats[row - 1][column - 1] = "Booked";
+	        System.out.println("Seat in Row " + row + ", Column " + column + " has been booked");
+
+	        // Save the updated booking status to the file
+	        try {
+	            saveBookingStatusB();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    } else {
+	        System.out.println("Invalid seat or seat already booked");
+	    }
 	}
 	
-	public void printSeatStatus() {
+	public void printSeatStatusA() {
+		for(int row = 0; row < 5; row++) {
+			for(int column = 0; column < 10; column++) {
+				System.out.println("Row" + (row + 1) + ", Column " + (column + 1) + ": " + trainSeats[row][column] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public void printSeatStatusB() {
 		for(int row = 0; row < 5; row++) {
 			for(int column = 0; column < 10; column++) {
 				System.out.println("Row" + (row + 1) + ", Column " + (column + 1) + ": " + trainSeats[row][column] + " ");
@@ -149,11 +210,12 @@ public class SeatsAvailable {
 	}
 	
 	// Load booking status from file
-    private void loadBookingStatus() throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader(bookingFilePath))) {
+    private void loadBookingStatusA() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(bookingFilePathA))) {
             String line;
             int row = 0;
             while ((line = reader.readLine()) != null && row < 5) {
+            	
                 String[] status = line.split(",");
                 for (int col = 0; col < 10 && col < status.length; col++) {
                     trainSeats[row][col] = status[col];
@@ -162,10 +224,41 @@ public class SeatsAvailable {
             }
         }
     }
+    
+    private void loadBookingStatusB() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(bookingFilePathB))) {
+            String line;
+            int row = 0;
+            while ((line = reader.readLine()) != null && row < 5) {
+            	
+                String[] status = line.split(",");
+                for (int col = 0; col < 10 && col < status.length; col++) {
+                    trainSeats[row][col] = status[col];
+                }
+                row++;
+            }
+        }
+    }
+    
 
     // Save booking status to a file
-    private void saveBookingStatus() throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(bookingFilePath))) {
+    private void saveBookingStatusA() throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(bookingFilePathA))) {
+            for (int row = 0; row < 5; row++) {
+                for (int col = 0; col < 10; col++) {
+                    writer.write(trainSeats[row][col]);
+                    if (col < 9) {
+                        writer.write(",");
+                    }
+                }
+                writer.newLine();
+            }
+        }
+    }
+
+    // Save booking status to a file
+    private void saveBookingStatusB() throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(bookingFilePathB))) {
             for (int row = 0; row < 5; row++) {
                 for (int col = 0; col < 10; col++) {
                     writer.write(trainSeats[row][col]);
@@ -178,7 +271,71 @@ public class SeatsAvailable {
         }
     }
 }
-//MainTest
+
+
+
+//MainTest for testing booking_statusA
+import java.io.IOException;
+
+import java.util.Scanner;
+
+public class MainTest {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		
+		SeatsAvailable seatsA = new SeatsAvailable(50);
+		SeatsAvailable seatsB = new SeatsAvailable(50);
+		
+		System.out.println("Welcome to WonderTrain Booking System!");
+		System.out.println("WE ARE PLEASANT TO SERVE OUR VALUABLE CUSTOMERS!");
+		
+			
+		System.out.println("Train Schedule of Tumpat-Gemas");
+		seatsA.displayTrainSchedulesA();
+				
+		System.out.println("Select A Train Model: ");
+		String trainModel = scanner.nextLine();
+				
+		System.out.println("Select departure Time: ");
+		String time = scanner.nextLine();
+				
+		System.out.println();
+				
+		System.out.println("The List Below shows the Available and Booked Seats:");
+		seatsA.displaySeatsTrainA();
+		System.out.print("Choose a Row : ");
+		int seatRow = scanner.nextInt();
+						
+		System.out.print("Choose a Column: ");
+		int seatColumn = scanner.nextInt();
+				
+				
+		System.out.println("Seat in Row " + seatRow + ", Column " + seatColumn + " is already booked. Please Choose Anohter Seat");
+				
+		System.out.println("Choose your Starting Station: ");
+		int beginStation = scanner.nextInt();
+					
+		System.out.println("Choose your Drop-off Station: ");
+		int dropStation = scanner.nextInt();
+					
+		int stationInterval = dropStation - beginStation;
+					
+		TrainFare fare = new TrainFare(stationInterval);
+		System.out.println("Total Fare:RM " + fare.computeFare());
+					
+		seatsA.bookSeatA(seatRow, seatColumn);
+		seatsA.printSeatStatusA();			
+	
+		
+	}
+
+}
+
+
+
+
+//MainTest for testing booking_statusB
 import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
@@ -203,3 +360,45 @@ public class MainTest {
 		seats.bookSeat(seatRow, seatColumn);
 		seats.printSeatStatus();
 	}
+
+
+
+
+
+
+
+import java.util.Scanner;
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+public class Main2 {
+
+	public static void main(String[] args) throws IOException {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		SeatsAvailable seatsB = new SeatsAvailable(50);
+		
+		System.out.println("Welcome to WonderTrain Booking System!");
+		System.out.println("WE ARE PLEASANT TO SERVE OUR VALUABLE CUSTOMERS!");
+		System.out.println();
+		
+		System.out.println("The List Below shows the Available and Booked Seats");
+		seatsB.displaySeatsTrainB();
+		System.out.print("Choose a Row : ");
+		int seatRow = scanner.nextInt();
+				
+		System.out.print("Choose a Column: ");
+		int seatColumn = scanner.nextInt();
+		
+		seatsB.isSeatBookedB(seatRow, seatColumn);
+		System.out.println("Seat in Row " + seatRow + ", Column " + seatColumn + " is already booked. Please Choose Anohter Seat");
+		
+		seatsB.bookSeatB(seatRow, seatColumn);
+		seatsB.printSeatStatusB();
+		
+		
+	}
+
+}
+
